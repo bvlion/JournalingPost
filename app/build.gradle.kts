a4ce18d3d9eb496bc75ae71fa92db712154c4293
bvlion <bvltiggeari@ambitious-i.net>
2026-09-04 09:35:08 +0900

    update version 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
     applicationId = "info.bvlion.journalingpost"
     minSdk = 31
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "1.0.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
